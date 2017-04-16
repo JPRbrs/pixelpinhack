ade71daf081f807229e3ec3d81f2108bd19f64b0
javier <perezrubio.javier@gmail.com>
2017-04-16 12:37:49 +0100

    Add jquery

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -1,3 +1,12 @@
+$(document).ready(function() {
+    $("#button").mouseenter(function(){
+	$("#button").fadeTo('fast', 0.5);
+    });
+    $("#button").mouseleave(function(){
+	$("#button").fadeTo('fast', 1);
+    });
+});
+
 var ractive = new Ractive({
     el: '#container',
     template: '#template',
